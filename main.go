0ba834271ae06c032a182769601ff65af0c9a508
FedericoIglesias <igl.fede@gmail.com>
2024-06-09 11:58:16 +0000

    initServer

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main (){
-	
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/users", UserServer)
+
+	log.Fatal((http.ListenAndServe(":8080", nil)))
+}
+
+
+
+func UserServer(w http.ResponseWriter, r *http.Request) {
+	var status int
+	switch r.Method {
+	case http.MethodGet:
+		status = 200
+		w.WriteHeader(status)
+		fmt.Fprintf(w, `{"satatus": %d, "message": "%s"}`, 200, "success in get")
+	case http.MethodPost:
+		status = 200
+		w.WriteHeader(status)
+		fmt.Fprintf(w, `{"satatus": %d, "message": "%s"}`, 200, "success in post")
+	default:
+		status = 404
+		w.WriteHeader(status)
+		fmt.Fprintf(w, `{"satatus": %d, "message": "%s"}`, 404, "not found")
+	}
 }
